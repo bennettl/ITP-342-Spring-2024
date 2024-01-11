eb1e399aed295096ca96a6b84e05e769c7a0b0b5
Bennett Lee <bennettlee@Bennetts-MacBook-Pro.local>
2024-01-10 19:41:12 -0800

    first week

---
+++ Week1/MyFirstApp/MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Bennett Lee on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Week1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+var greeting = "Hello, playground"
+1+2
